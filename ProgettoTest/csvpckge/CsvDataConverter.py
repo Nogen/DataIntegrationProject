3ec8b8e90c7fb2e1c75eeaa22109f05225a6d8dc
noh <aeonss@live.com>
2018-09-19 01:39:52 +0200

    writing tests

--- ProgettoTest/csvpckge/CsvDataConverter.py
+++ ProgettoTest/csvpckge/CsvDataConverter.py
@@ -11,10 +11,19 @@ class CsvDataConverter:
 
 
     def __init__(self, row, elmtypes):
-        self.row = row
-        self.elmtype = elmtypes
+        """
+            Args:
+                row (string array): elementi di una riga del Csv
+                elmtypes (string array): tipo degli elemnti della riga del csv
+        """
+        self.row = map(lambda x: str(x), row)
+        self.elmtype = map(lambda x: str(x), elmtypes)
 
     def values2Query(self):
+        """
+            Returns:
+                una stringa degli elementi puliti pronti per una query
+        """
         str = ""
         for i in range(len(self.row)):
             str += CsvDataConverter.MAP[self.elmtype[i]](self.row[i]) + ","
