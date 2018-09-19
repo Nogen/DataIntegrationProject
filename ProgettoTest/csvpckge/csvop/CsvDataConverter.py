a76e8c7a0b212245bd16bd2fccd050a4fe2a52f8
noh <aeonss@live.com>
2018-09-20 00:47:34 +0200

    test fatti per csvDataConverter

--- ProgettoTest/csvpckge/csvop/CsvDataConverter.py
+++ ProgettoTest/csvpckge/csvop/CsvDataConverter.py
@@ -3,8 +3,9 @@ from csvpckge.util import strFilter
 class CsvDataConverter:
 
     TIME = " 23:12:18"
-    MAP = {"INT" : (lambda x: strFilter(x).sanitizeDouble()  or "NULL"),
-            "DOUBLE" : (lambda x: strFilter(x).sanitizeDouble()  or "NULL") ,
+    NULL = "NULL"
+    MAP = {"INT" : (lambda x: strFilter(x).sanitizeDouble()  or CsvDataConverter.NULL),
+            "DOUBLE" : (lambda x: strFilter(x).sanitizeDouble()  or CsvDataConverter.NULL) ,
              "DATE" : (lambda x: x + CsvDataConverter.TIME),
              "VARCHAR(256)" : (lambda x:  "\"" +  strFilter(x, "\"").sanitize() + "\"")
     }
@@ -16,8 +17,13 @@ class CsvDataConverter:
                 row (string array): elementi di una riga del Csv
                 elmtypes (string array): tipo degli elemnti della riga del csv
         """
+        if (not row or not elmtypes):
+            raise ValueError("args cannot be null")
+        if (len(row) != len(elmtypes)):
+            raise ValueError("Args's length should be equal")
         self.row = map(lambda x: str(x), row)
         self.elmtype = map(lambda x: str(x), elmtypes)
+
 
     def values2Query(self):
         """
@@ -26,5 +32,8 @@ class CsvDataConverter:
         """
         str = ""
         for i in range(len(self.row)):
-            str += CsvDataConverter.MAP[self.elmtype[i]](self.row[i]) + ","
+            try:
+                str += CsvDataConverter.MAP[self.elmtype[i]](self.row[i]) + ","
+            except KeyError:
+                pass
         return str[:-1]
--- ProgettoTest/csvpckge/tests/csvConvtestBB.py
+++ ProgettoTest/csvpckge/tests/csvConvtestBB.py
@@ -2,4 +2,39 @@ import unittest
 from ..csvop.CsvDataConverter import CsvDataConverter
 
 class TestCsvConverter(unittest.TestCase):
-    pass
+
+    def testRowVuoto(self):
+        row = []
+        elemtypes = ["INT", "DOUBLE"]
+        with self.assertRaises(ValueError) as fail:
+            conv = CsvDataConverter(row, elemtypes)
+
+    def testElemVuoto(self):
+        row = ["3", "4.0"]
+        elemtypes = []
+        with self.assertRaises(ValueError) as fail:
+            conv = CsvDataConverter(row, elemtypes)
+
+    def testLunghezze(self):
+        row = ["6", "4"]
+        elemtypes = ["INT", "VARCHAR(256)", "DATE"]
+        with self.assertRaises(ValueError) as fail:
+            conv = CsvDataConverter(row, elemtypes)
+
+    def testElementinonContenuti(self):
+        row = ["3", "stringa", "4.0", "1994-04-03"]
+        elemtypes = ["INT", "TEXT", "DOUBLE", "TIME"]
+        conv = CsvDataConverter(row, elemtypes)
+        final = "%s,%s" % (row[0], row[2])
+        self.assertEqual(conv.values2Query(), final)
+
+    def testElementiCorretti(self):
+        row = ["3", "stringa", "4.0", "1994-04-03"]
+        elemtypes = ["INT", "VARCHAR(256)", "DOUBLE", "DATE"]
+        conv = CsvDataConverter(row, elemtypes)
+        final = "%s,\"%s\",%s,%s%s" % (row[0], row[1], row[2], row[3], CsvDataConverter.TIME)
+        self.assertEqual(conv.values2Query(), final)
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCsvConverter)
+    unittest.TextTestRunner(verbosity=2).run(suite)
